27a7d65af817c99f3bcc3543c79541e7f5cac536
leudi <leudi_rosario@hotmail.com>
2020-08-19 01:30:08 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const MyTextWithProp = ({ style }) => {
+  const [texto, setTexto] = useState('Hola Mundo')
+  const hanlderClick = () => {
+    setTexto('Boton Clicked')
+  }
+  // const { msg } = props;
+  return (<Text style={[styles.texto, style]} onPress={hanlderClick}>{texto}</Text>);
+}
+
+
+const MyTextChildren = (props) => {
+  const { children } = props;
+  return (<Text >{children}</Text>);
+}
+
+
 
 export default function App() {
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyTextWithProp style={styles.white} />
+      <MyTextWithProp style={styles.red} />
+      <MyTextWithProp style={styles.green} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  texto: {
+    fontSize: 20, fontWeight: 'bold', color: 'white'
+
+  },
+  red: {
+    backgroundColor: 'red'
+  },
+  green: {
+    backgroundColor: 'green',
+  },
+  white: {
+    backgroundColor: 'black',
+  },
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
   },
 });
