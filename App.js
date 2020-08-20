27ca599ad44f01aa53e404c99345b59bed470a24
leudi <leudi_rosario@hotmail.com>
2020-08-20 13:59:58 -0400

    Agregando TextInput

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableHighlight } from 'react-native';
 
 
 const MyTextWithProp = ({ style }) => {
@@ -9,52 +9,66 @@ const MyTextWithProp = ({ style }) => {
     setTexto('Boton Clicked')
   }
   // const { msg } = props;
-  return (<Text style={[styles.texto, style]} onPress={hanlderClick}>{texto}</Text>);
+  return (<Text style={[styles.texto, style]}
+    onPress={hanlderClick}
+
+
+
+  >{texto}</Text>);
+}
+const handleSubmit = () => {
+  console.log(texto)
+  settexto('')
+  alert(texto)
 }
 
 
-const MyTextChildren = (props) => {
-  const { children } = props;
-  return (<Text >{children}</Text>);
-}
 
+// const MyTextChildren = (props) => {
+//   const { children } = props;
+//   return (<Text >{children}</Text>);
+// }
 
+{/* <MyTextWithProp style={styles.white} />
+      <MyTextWithProp style={styles.red} />
+      <MyTextWithProp style={styles.green} /> */}
 
 export default function App() {
-
-
-
-
-
+  const [texto, settexto] = useState('')
   return (
     <View style={styles.container}>
-      <MyTextWithProp style={styles.white} />
-      <MyTextWithProp style={styles.red} />
-      <MyTextWithProp style={styles.green} />
+      {/* <MyTextWithProp style={styles.white} ></MyTextWithProp> */}
+      <Text>Texto Digitado: {texto}</Text>
+      <TextInput
+        style={styles.Input}
+        placeholder='Digita tu nombre'
+        onChange={t => settexto(t.target.value)}
+        defaultValue={texto}
+      ></TextInput>
+      <Button title='Aceptar'
+        onPress={handleSubmit}
+
+      ></Button>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  texto: {
-    fontSize: 20, fontWeight: 'bold', color: 'white'
-
-  },
-  red: {
-    backgroundColor: 'red'
-  },
-  green: {
-    backgroundColor: 'green',
-  },
-  white: {
-    backgroundColor: 'black',
+  Input: {
+    fontSize: 24,
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    marginBottom: 12
   },
   container: {
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
